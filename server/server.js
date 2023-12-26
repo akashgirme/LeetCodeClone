36f5edb5c7e7be53db1ae988a42d723849851f99
akashgirme <akash17g@gmail.com>
2023-12-26 23:03:23 +0530

    a

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.use(cookieParser());
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", clientURL);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
